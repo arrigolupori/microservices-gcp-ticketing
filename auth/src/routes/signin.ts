e4e525c86deb83aad08ca5f36799d71e30d88c6d
Arrigo Lupori <arrigo.lupori@gmail.com>
2023-11-21 20:27:21 +0100

    destructured common utilities into npm package

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,7 @@
 import express, { type Request, type Response } from "express";
 import { body } from "express-validator";
-import { validateRequest } from "../middlewares/validate-request";
+import { validateRequest, BadRequestError } from "@sagewillcom/common";
 import { User } from "../models/user";
-import { BadRequestError } from "../errors/bad-request-error";
 import { Password } from "../services/password";
 import jwt from "jsonwebtoken";
 
