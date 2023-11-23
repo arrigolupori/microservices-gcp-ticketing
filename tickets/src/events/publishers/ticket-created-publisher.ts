b5592f07859e0a57e0518ffabc643f2f3ffd3b60
Arrigo Lupori <arrigo.lupori@gmail.com>
2023-11-23 23:31:32 +0100

    termination logic for nats wrapper in tickets

--- tickets/src/events/publishers/ticket-created-publisher.ts
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -1,2 +1,5 @@
-import { Publisher, Subjects, TicketCreatedEvent } from "@sagewillcom/common";
+import { Publisher, Subjects, type TicketCreatedEvent } from "@sagewillcom/common";
 
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+}
