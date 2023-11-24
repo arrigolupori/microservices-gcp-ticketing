8d23a4a73a2fcad2b56bf8329377a37c73fc39b3
Arrigo Lupori <arrigo.lupori@gmail.com>
2023-11-24 02:21:09 +0100

    set up route handlers for orders service

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,6 +4,11 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@sagewillcom/common";
 
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes";
+import { deleteOrderRouter } from "./routes/delete";
+
 const app = express();
 
 app.set("trust proxy", true);
@@ -16,6 +21,11 @@ app.use(
 );
 
 app.use(currentUser);
+
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
