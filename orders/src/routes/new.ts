88630a96ce31e1680a481aa087723328313720e3
Arrigo Lupori <arrigo.lupori@gmail.com>
2023-11-26 15:01:04 +0100

    order and ticket models in order service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
+import mongoose from "mongoose";
 import express, { type Request, type Response } from "express";
+import { requireAuth, validateRequest } from "@sagewillcom/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Ticket ID must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
