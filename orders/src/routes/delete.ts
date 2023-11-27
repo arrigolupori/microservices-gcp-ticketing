333159f48d6e1294dca01de88bde7674346a3e0d
Arrigo Lupori <arrigo.lupori@gmail.com>
2023-11-27 21:43:48 +0100

    added event publishers and event test to orders service

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -6,6 +6,8 @@ import {
 } from "@sagewillcom/common";
 import express, { type Request, type Response } from "express";
 import { Order } from "../models/order";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -15,7 +17,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate("ticket");
 
     if (!order) {
       throw new NotFoundError();
@@ -29,7 +31,14 @@ router.delete(
 
     await order.save();
 
-    res.send(order);
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    res.status(204).send(order);
   }
 );
 
